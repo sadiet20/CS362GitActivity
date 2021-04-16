7ca6c16e86a8a1859db6ac4890b788bf029258e5
sadiet20 <thomsadi@flip1.engr.oregonstate.edu>
2021-04-15 17:18:08 -0700

    generated password

--- pass.py
+++ pass.py
@@ -1,6 +1,11 @@
+import random
+
 
 def createpassword(length):
-	pass
+	password = ""
+	for i in range(length):
+		password += chr(random.randrange(97, 122))
+	print("Your password is:", password)
 
 
 createpassword(8)
